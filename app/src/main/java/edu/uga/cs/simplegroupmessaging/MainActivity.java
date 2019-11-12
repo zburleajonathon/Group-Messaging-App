3002a78ae6d2ac6f2d215f2bfa7bd71ee90084fb
kayin60 <jmz12217@uga.edu>
2019-11-11 21:21:21 -0500

    Main splash page and signup funtionality completed

--- app/src/main/java/edu/uga/cs/simplegroupmessaging/MainActivity.java
+++ app/src/main/java/edu/uga/cs/simplegroupmessaging/MainActivity.java
@@ -2,14 +2,37 @@ package edu.uga.cs.simplegroupmessaging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button signInButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.signUpButton);
+        signUpButton = findViewById(R.id.button2);
+
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/edu/uga/cs/simplegroupmessaging/SignUpActivity.java
+++ app/src/main/java/edu/uga/cs/simplegroupmessaging/SignUpActivity.java
@@ -1,24 +1,63 @@
 package edu.uga.cs.simplegroupmessaging;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    public SignUpActivity() {
-        super();
-    }
+    DatabaseReference dbRef;
+    EditText email, password;
+    Button signUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
         // ...
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.signUpEmailEditText);
+        password = findViewById(R.id.signUpPassEditText);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailText = email.getText().toString();
+                String passText = password.getText().toString();
+
+                if(TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passText)){
+                    Toast.makeText(SignUpActivity.this, "Either the email or password fields are empty.", Toast.LENGTH_SHORT).show();
+                }
+                else if(passText.length() < 5){
+                    Toast.makeText(SignUpActivity.this, "The password provided is too short.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    signUp(emailText, passText);
+                }
+            }
+        });
+
+
+
     }
 
     @Override
@@ -36,6 +75,50 @@ public class SignUpActivity extends AppCompatActivity {
         }
         else {
         }
+    }
+
+    public void signUp(final String email, String pass){
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            String userID = user.getUid();
+                            dbRef = FirebaseDatabase.getInstance().getReference("Users").child(userID);
+
+                            //Create HashMap to store info into database with a single call
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userID);
+                            hashMap.put("username", email);
+
+                            //add to database
+                            dbRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 
 }
