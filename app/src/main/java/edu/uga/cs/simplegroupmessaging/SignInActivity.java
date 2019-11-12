a8b847283630b376f301ec791cfd6fe93fb29fcd
kayin60 <jmz12217@uga.edu>
2019-11-12 18:28:35 -0500

    Added Sign In Functionality

--- app/src/main/java/edu/uga/cs/simplegroupmessaging/SignInActivity.java
+++ app/src/main/java/edu/uga/cs/simplegroupmessaging/SignInActivity.java
@@ -1,7 +1,91 @@
 package edu.uga.cs.simplegroupmessaging;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    DatabaseReference dbRef;
+    EditText email, password;
+    Button signUpButton;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.signUpEmailEditText);
+        password = findViewById(R.id.signUpPassEditText);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailText = email.getText().toString();
+                String passText = password.getText().toString();
+
+                if(TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passText)){
+                    Toast.makeText(SignInActivity.this, "Either the email or password fields are empty.", Toast.LENGTH_SHORT).show();
+                }
+                else if(passText.length() < 5){
+                    Toast.makeText(SignInActivity.this, "The password provided is too short.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    email.setText("");
+                    password.setText("");
+                    signIn(emailText, passText);
+
+                    //Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                    //startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+        //hideProgressDialog();
+        if (user != null) {
+        }
+        else {
+        }
+    }
+
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+
+                            Toast.makeText(SignInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
 
 }
